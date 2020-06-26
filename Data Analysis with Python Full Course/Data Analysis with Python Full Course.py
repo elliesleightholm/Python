0ae313dd05b6cdb63f9dc2bdbf24a62ac8e1804a
Ellie Sleightholm <59167495+elliesleightholm@users.noreply.github.com>
2020-06-26 17:19:39 +0100

    Add files via upload

---
+++ Data Analysis with Python Full Course/Data Analysis with Python Full Course.py
@@ -0,0 +1,12 @@
+
+''' DATA ANALYSIS WITH PYTHON - FULL COURSE '''
+
+# What is Data Analysis?
+# "A process of inspectiing, cleansing, transforming and modelling data with
+# the goal of discovering useful information, informing conclusion and
+# supporting decision-making"
+
+# Throughout this course we use Jupyter notebooks. For ease, I exported them
+# as pdfs - this was just my personal preference. I retain information 
+# better when I repeat the code instead of copying and pasting. Exporting 
+# them as pdfs helps prevent me from the 'easy way out' as it were! 
