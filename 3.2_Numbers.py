7bc1515a288070b2a6087aa7b52b0f22a4dd885f
Ellie Sleightholm <59167495+elliesleightholm@users.noreply.github.com>
2020-06-26 10:51:16 +0100

    Add files via upload

---
+++ 3.2_Numbers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 11:16:42 2020
+
+@author: elliesleightholm
+"""
+
+## 2 - Numbers 
+# Generic maths commands
+
+1 + 2 # Addition
+5 * 3 # Multiplication
+5 - 2 # Subtraction
+5 / 2 # Division
+
+7 % 4 
+50 % 5  # Modulo
+
+2 ** 3 # Powers
+
+
+
